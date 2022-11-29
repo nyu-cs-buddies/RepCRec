22dff8283d76c1a37f5ce2c167e358945c4202f6
Che-Hsien Chiu <qqqccl5678@gmail.com>
2022-11-29 13:57:20 -0500

    seperate lockMng to .cpp and headerfile. Create site.h and site.cpp

---
+++ src/lockManager.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include "lockManager.h"
+using namespace std;
+
+void LockManager::requestRLock(int transactionId, int varIdx, int& lockHolder){
+    // check whether a writelock on it
+    if (!WLockTable.count(varIdx)) {
+        // provide a RLock
+        if (RLockTable.count(varIdx)) {
+            RLockTable[varIdx].transactionIds.insert(transactionId);
+        } 
+        else {
+            ReadLock readLock;
+            readLock.transactionIds.insert(transactionId);
+            RLockTable[varIdx] = readLock;
+        }
+        return;
+    }
+    // else block this transaction
+    lockHolder = WLockTable[varIdx].transactionId;
+    return;
+}
+
+void LockManager::requestWLock(const int transactionId, const int varIdx, unordered_set<int>& lockHolders){
+    // check whether a readlock on it
+    if (!RLockTable.count(varIdx) && !WLockTable.count(varIdx)) {
+        // provide a WLock
+        WriteLock writeLock;
+        writeLock.transactionId = transactionId;
+        WLockTable[varIdx] = writeLock;
+        return;
+    }
+    // else block this transaction
+    if (RLockTable.count(varIdx)) {
+        auto ids = RLockTable[varIdx].transactionIds;
+        lockHolders.insert(ids.begin(), ids.end());
+    } else {
+        lockHolders.insert(WLockTable[varIdx].transactionId);
+    }
+    return;
+}
+
+
+void LockManager::promoteLock(const int transactionId, const int idx) {
+    
+    RLockTable.erase(idx);
+    WriteLock writeLock;
+    writeLock.transactionId = transactionId;
+    WLockTable[idx] = writeLock;
+}
+
+list<int> LockManager::releaseLock(const int transactionId) {
+    // check ReadLock
+    list<int> tmp;  // use to update ReadLock table
+    for (auto& r : RLockTable) {
+        r.second.transactionIds.erase(transactionId);
+        if (r.second.transactionIds.empty()) {
+            tmp.push_back(r.first);
+        }
+    }
+
+    // clean up RLockTable
+    for (const auto& i : tmp) {
+        RLockTable.erase(i);
+    }
+
+    // check WriteLock
+    list<int> modifiedVar;
+    for (auto& w : WLockTable) {
+        if (w.second.transactionId == transactionId) {
+            modifiedVar.push_back(w.first);
+        }
+    }
+
+    // clean up WLockTable
+    for (const auto& i : modifiedVar) {
+        WLockTable.erase(i);
+    }
+    return modifiedVar;
+}
+
+void LockManager::releaseAllLock() {
+    RLockTable.clear();
+    WLockTable.clear();
+}
+
+void LockManager::dump() const {
+    cout << "RLock Holders: ";
+    for (const auto& r : RLockTable) {
+        cout << r.first << " : ";
+        for (const auto& t : r.second.transactionIds) {
+        cout << t << " ";
+        }
+        cout << " || ";
+    }
+    cout << endl;
+
+    cout << "WLockHolders: ";
+    for (const auto& w : WLockTable) {
+        cout << w.first << " : " << w.second.transactionId << " || ";
+    }
+    cout << endl;
+}
+
+
---
+++ src/site.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <string>
+#include "site.h"
+using namespace std;
+
+Site::Site(const int id) : id(id), siteStatus(SiteStatus::UP) { initialize(); }
+
+bool Site::read(const int transactionId, const int idx, int& lockHolder, int& readVal) {
+    if (siteStatus == SiteStatus::DOWN || !commitedVal.count(idx)) {
+        // site is down or variable does not exit on this site
+        return false;
+    }
+
+    if (idx % 2 == 0 && restrictedReadVariable.count(idx)) {
+        // if the site just recovered, we can not read the replicated variables
+        // until they are commited
+        return false;
+    }
+
+    // request a ReadLock for read variable
+    lockManager.requestRLock(transactionId, idx, lockHolder);
+    readVal = lockHolder == transactionId ? curVal[idx] : commitedVal[idx];
+    return true;
+}
+
+bool Site::write(const int transactionId, const int idx, const int varVal, unordered_set<int>& lockHolders) {
+    if (siteStatus == SiteStatus::DOWN || !commitedVal.count(idx)) {
+        // site is down or variable does not exit on this site
+        return false;
+    }
+
+    if (restrictedWriteVariable.count(idx)) {
+        return false;  
+    }
+
+    // request a WLock for write variable
+    lockManager.requestWLock(transactionId, idx, lockHolders);
+
+    // if already has a read lock, RLock promotes to WLock
+    if (lockHolders.count(transactionId) && lockHolders.size() == 1) {
+        // promote RLock to WLock
+        lockManager.promoteLock(transactionId, idx);
+        lockHolders.clear();
+    }
+    if (lockHolders.empty()) {
+        // allow to write to curVal
+        curVal[idx] = varVal;
+        return true;
+    }
+
+    return false;
+}
+
+
+void Site::abort(const int transactionId) {
+    auto modifiedVar = lockManager.releaseLock(transactionId);
+    for (const auto& var : modifiedVar) {
+        curVal.erase(var);
+        restrictedWriteVariable.erase(var);
+    }
+    return;
+}
+
+void Site::commit(const int transactionId, const unordered_set<int>& affectedVariables) {
+    lockManager.releaseLock(transactionId);
+    for (const auto& affectedVar : affectedVariables) {
+        if (curVal.count(affectedVar)) {
+            commitedVal[affectedVar] = curVal[affectedVar];
+            curVal.erase(affectedVar);
+            // if the site just recovered, we need to clean up restrictedReadVariable
+            // to make it readable
+            restrictedReadVariable.erase(affectedVar);
+        }
+        restrictedWriteVariable.erase(affectedVar);
+    }
+}
+
+bool Site::fail(int time) {
+    if (siteStatus != SiteStatus::UP) {
+        cout << "Site" << id << " is already DOWN!" << endl;
+        return false;
+    }
+    lockManager.releaseAllLock();
+    curVal.clear();
+    siteStatus = SiteStatus::DOWN;
+    failedTime = time;
+    return true;
+}
+
+bool Site::recover() {
+    if (siteStatus != SiteStatus::DOWN) {
+        cout << "Site" << id << " is already UP!" << endl;
+        return false;
+    }
+    // initialize variables
+    initialize();
+
+    // mark rpelicated variables as restricted
+    for (int i = 2; i <= 20; i += 2) {
+        restrictedReadVariable.insert(i);
+    }
+    siteStatus = SiteStatus::UP;
+    return true;
+}
+
+void Site::dump() const {
+    string delim = "";
+    cout << "Site " << id << " -";
+    for (const auto& v : commitedVal) {
+        cout << delim << " x" << v.first << ": " << v.second;
+        delim = ",";
+    }
+    cout << endl;
+
+    // debug purpose
+    // cout << "============" << endl;
+    // cout << "Current Val" << endl;
+    // cout << "============" << endl;
+    // delim = "";
+    // cout << "site " << id << " -";
+    // for (const auto& [idx, val] : curVal) {
+    //   cout << delim << " x" << idx << ": " << val;
+    //   delim = ",";
+    // }
+    // cout << endl;
+    // cout << "============" << endl;
+    // cout << "LockTable" << endl;
+    // cout << "============" << endl;
+    // lockManager.dump();
+    // cout << endl;
+}
+
+void Site::initialize() {
+    // save even indexed variables (replicated variables)
+    for (int i = 2; i <= 20; i += 2) {
+        commitedVal[i] = 10 * i;
+    }
+
+    // save odd indexed variables (non-replicated variables)
+    for (int i = 1; i <= 19; i += 2) {
+        if (id == (i % 10) + 1) {
+            commitedVal[i] = 10 * i;
+        }
+    }
+}
